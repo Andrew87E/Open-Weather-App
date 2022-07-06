66977e1d01ee59756b1e0b96a47ef9d6cf51af0b
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-07-06 02:02:29 -0500

    changed cards, fixed sidebar, broke sidebar again

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,77 @@
-var apiKey = '46b9fbe392a7416271fab6f07e46740a'
-var cityName = localStorage.getItem('cityName')
-var weatherAPIUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=46b9fbe392a7416271fab6f07e46740a`
-var geoAPIUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKey}`
-var listEl = $('#ae-list');
-var userInput = $('#userInput');
+var apiKey = "46b9fbe392a7416271fab6f07e46740a";
+var cityName = localStorage.getItem("cityName");
+var geoAPIUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKey}`;
+var cardBackCity = `https://maps.googleapis.com/maps/api/place/photo?parameters`;
 var lat;
 var lon;
+var listEl = $("#ae-list");
+var userInput = $("#userInput");
 
 
-$('#userInput').keypress(function (event) {
-    if (event.key === 'Enter') {
-        if (userInput === ' ' || userInput === null) {
-            window.alert('Please enter your initials. Press enter when finished.')
-        } else {
-            userInput = (this).value;
-            localStorage.setItem('cityName', userInput);
-        }
+fetch(geoAPIUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    console.log("lat", data[0].lat);
+    console.log("lon", data[0].lon);
+    lat = data[0].lat;
+    lon = data[0].lon;
+  })
+  .then(function () {
+    var weatherAPIUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    fetch(weatherAPIUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+  });
+
+  $("#userInput").keypress(function (event) {
+    if (event.key === "Enter") {
+      if (userInput === " " || userInput === null) {
+        window.alert("Please enter your initials. Press enter when finished.");
+      } else {
+        userInput = this.value;
+        localStorage.setItem("cityName", userInput);
+        addListItem();
+      }
     }
-});
+  });
+
+function addListItem() {
+  if (cityName === $('#ae-list')){
+        return;
+    }else {
+        listEl.append('<li>' + '<a href="#">' + cityName + '</a>');        
+        }
+}
 
 getLatLon();
-console.log(cityName)
-
+console.log(cityName);
 function getLatLon() {
-    fetch(geoAPIUrl)
-        .then(function (response) {
-            return response.json();
-        }).then(function (data) {
-            console.log(data);
-            console.log('lat', data[0].lat);
-            console.log('lon', data[0].lon);
-            lat = data[0].lat.text();
-            lon = data[0].lon.text();
-        })
-};
+    
 
-console.log(lat)
-console.log(lon)
-
-currentForecast()
-
+console.log(lat);
+console.log(lon);
+}
 function currentForecast() {
-    fetch(weatherAPIUrl)
-        .then(function (response) {
-            return response.json();
-        }).then(function (data) {
-            console.log(data);
-            // append to page here
-        })
-};
+  console.log(lat);
+  console.log(lon);
+  fetch(weatherAPIUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      // append to page here
+    });
+}
+
+$(".option").click(function(){
+    $(".option").removeClass("active");
+    $(this).addClass("active");
+ });
