8db312143520cd7ce3a1204c76e03c695b198c3c
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-07-06 20:20:37 -0500

    fixed some bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,96 +6,75 @@ var lat;
 var lon;
 var listEl = $("#ae-list");
 var userInput = $("#userInput");
+var currentCityEl = $('.current-city')
 var weatherData;
 var usableData;
 var weatherCurrent;
 var weatherDaily;
 var weatherHourly;
 
-
 fetch(geoAPIUrl)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
-    console.log("lat", data[0].lat);
-    console.log("lon", data[0].lon);
     lat = data[0].lat;
     lon = data[0].lon;
   })
   .then(function () {
     var weatherAPIUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
     fetch(weatherAPIUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      weatherCurrent = data.current;
-      weatherDaily = data.daily;
-      weatherHourly = data.hourly;
-      console.log(data.current);
-    })
-  })
-
-  // .then(console.log(weatherDaily));
-  // .then(console.log(weatherHourly));
-  // .then(console.log(weatherCurrent));
-
-
-  $("#userInput").keypress(function (event) {
-    if (event.key === "Enter") {
-      if (userInput === " " || userInput === null) {
-        window.alert("Please enter your initials. Press enter when finished.");
-      } else {
-        userInput = this.value;
-        localStorage.setItem("cityName", userInput);
-        addListItem();
-        generateWeather();
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        weatherCurrent = data.current;
+        weatherDaily = data.daily;
+        weatherHourly = data.hourly;
+        console.log(weatherCurrent);
         console.log(data)
-      }
-    }
+        generateCurrentWeather()
+
+      });
   });
 
-function addListItem() {
-  if (cityName === $('#ae-list')){
-        return;
-    }else {
-        listEl.append('<li>' + '<a href="#">' + cityName + '</a>');        
-        }
-}
-
-getLatLon();
-console.log(cityName);
-function getLatLon() {
-    
-
-}
-function currentForecast() {
-  console.log(lat);
-  console.log(lon);
-  fetch(weatherAPIUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
+$("#userInput").keypress(function (event) {
+  if (event.key === "Enter") {
+    if (userInput === " " || userInput === null) {
+      window.alert("Please enter your initials. Press enter when finished.");
+    } else {
+      userInput = this.value;
+      localStorage.setItem("cityName", userInput);
+      addListItem();
+      generateWeather();
       console.log(data);
-      // append to page here
-    });
+    }
+  }
+});
+
+function addListItem() {
+  listEl.append("<li>" + '<a href="#">' + cityName + "</a>" + "</li>");
+};
+
+function generateCurrentWeather() {
+  currentCityEl.append('<h1>' + cityName + '</h1>')
+  $('.city-info').append('<p>' + 'current temp is : ' + weatherCurrent.temp + ' it feels like: ' + 
+  weatherCurrent.feels_like + ' The humidity is: ' + weatherCurrent.humidity + '</p>');
+
 }
 
-$(".option").click(function(){
-    $(".option").removeClass("active");
-    $(this).addClass("active");
- });
+$(".option").click(function () {
+  $(".option").removeClass("active");
+  $(this).addClass("active");
+});
 
- function generateBackground(){
+function generateBackground() {
   //if weather = sunny {insert sunny background and icon}
   //else if weather === rain {insert rainy background}
   //else if weather === cloudy {insert cloudy background}
   // else if
- }
+}
 
- function generateForecastCard(){
-  //for loop iterate through built array of data 
- }
+function generateForecastCard() {
+  //for loop iterate through built array of data
+}
