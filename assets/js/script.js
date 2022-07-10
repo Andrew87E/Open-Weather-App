9057cb32b200533122479d528f54f25970a80371
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-07-09 22:32:59 -0500

    turn in ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,21 @@ var weatherDaily;
 var weatherHourly;
 var todaysDate = moment().format("dddd, MMMM, YYYY, h:mm a");
 
+function init() {
+  var $cardContainer = $(".ae-container");
+
+  //create our forecast cards
+  for (var day = 0; day < 5; day++) {
+    var template = `
+      <div class="forecast-card">
+          <div class="forecast-card__cover" id="day-${day}"></div>
+      </div>`;
+
+    var $card = $(template);
+    $cardContainer.append($card);
+  }
+}
+
 function generateGeo() {
   fetch(geoAPIUrl)
     .then(function (response) {
@@ -34,7 +49,6 @@ function generateGeo() {
         .then(function (data) {
           weatherCurrent = data.current;
           weatherDaily = data.daily;
-          console.log(weatherDaily);
           generateCurrentWeather();
         });
     });
@@ -42,24 +56,24 @@ function generateGeo() {
 
 $(".default").on("click", function () {
   cityName = this.text;
-  console.log(this.text);
-  currentCityEl.text("");
-  $(".forecast-card__cover").text("");
+  eraser()
   generateGeo();
   geoAPIUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKey}`;
 });
 
+function eraser() {
+  currentCityEl.text("");
+  $(".forecast-card__cover").text("");
+}
+
 userInput.keypress(function (event) {
-  if (listEl) {
-  }
   if (event.key === "Enter") {
-    currentCityEl.text("");
+    eraser()
     userInput = this.value;
     cityName = this.value;
     localStorage.setItem("cityName", userInput);
     addListItem();
     generateGeo();
-    // console.log(data);
   }
 });
 
@@ -70,295 +84,67 @@ $("#uvi").hide();
 
 function generateCurrentWeather() {
   $("#uvi").show();
-  currentCityEl.append(
-    "<h1>" +
-      "The current weather in  " +
-      cityName +
-      " " +
-      todaysDate +
-      "</h1>" +
-      "<br>"
-  );
-    console.log(weatherCurrent)
-  currentCityEl.append(
-    "<h3 class='current-h3'>" +
-    "It's " +
-    weatherCurrent.weather[0].main +
-    " outside! " +
-    "</h3>" +
-    "<p>" +
-      "The current temp is : " +
-      weatherCurrent.temp +
-      "<br>" +
-      " It feels like: " +
-      weatherCurrent.feels_like +
-      "<br>" +
-      " The humidity is: " +
-      weatherCurrent.humidity +
-      "<br>" +
-      " The wind speed is " +
-      weatherCurrent.wind_speed +
-      " mph " +
-      "<br>" +
-      "<br>" +
-      "</p>" +
-      '<p id="uvi">' +
-      "The current UV Index is " +
-      weatherCurrent.uvi +
-      "</p>"
-  );
+
+  currentCityEl.append(`
+    <h1>
+      The current weather in  ${cityName}<br>
+      ${todaysDate}
+    </h1><br>
+    <p>
+      The current temp is: ${weatherCurrent.temp}<br>
+      It feels like: ${weatherCurrent.feels_like}<br>
+      The humidity is: ${weatherCurrent.humidity}<br>
+      The wind speed is: ${weatherCurrent.wind_speed}mph<br>    
+      <br><span id="uvi">The current UV Index is: ${weatherCurrent.uvi}</span>
+    </p>
+  `);
 
   var uviEl = $("#uvi");
 
-  if (weatherCurrent.uvi < 3) {
-    uviEl.addClass("lowuvi");
-  } else if (weatherCurrent.uvi > 3 && weatherCurrent.uvi < 6) {
+  if (weatherCurrent.uvi < 3) uviEl.addClass("lowuvi");
+  else if (weatherCurrent.uvi > 3 && weatherCurrent.uvi < 6)
     uviEl.addClass("meduvi");
-  } else if (weatherCurrent.uvi > 6 && weatherCurrent.uvi < 8) {
+  else if (weatherCurrent.uvi > 6 && weatherCurrent.uvi < 8)
     uviEl.addClass("moderateuvi");
-  } else if (weatherCurrent.uvi > 8 && weatherCurrent.uvi < 11) {
+  else if (weatherCurrent.uvi > 8 && weatherCurrent.uvi < 11)
     uviEl.addClass("highuvi");
-  } else {
-    uviEl.addClass("yougonnadie");
-  }
-  console.log("uviEL", uviEl);
+  else uviEl.addClass("yougonnadie");
+
   uviEl.text("The current UV Index is " + weatherCurrent.uvi);
+
+  var icon = weatherCurrent.weather[0].icon;
+  currentCityEl.css("background-image", `url(./assets/img/${icon}.gif)`);
+
   generateForecastCard();
 }
 
 function generateForecastCard() {
-  $("#day-0").append(
-    "<h1>" +
-      moment().add(1, "day").format("dddd") +
-      " " +
-      "</h1>" +
-      "<p id=futureWeather>" +
-      " It will be: " +
-      weatherDaily[0].weather[0].description +
-      "<br>" +
-      "<br>" +
-      "The high will be : " +
-      weatherDaily[0].temp.max +
-      "<br>" +
-      "The low will be : " +
-      weatherDaily[0].temp.min +
-      "<br>" +
-      "<br>" +
-      " The humidity will be : " +
-      weatherDaily[0].humidity +
-      "<br>" +
-      "<br>" +
-      "The UV Index will be " +
-      weatherDaily[0].uvi +
-      "</p>"
-  );
-  $("#day-1").append(
-    "<h1>" +
-      moment().add(2, "day").format("dddd") +
-      " " +
-      "</h1>" +
-      "<p id=futureWeather>" +
-      " It will be: " +
-      weatherDaily[1].weather[0].description +
-      "<br>" +
-      "<br>" +
-      "The high will be : " +
-      weatherDaily[1].temp.max +
-      "<br>" +
-      "The low will be : " +
-      weatherDaily[1].temp.min +
-      "<br>" +
-      "<br>" +
-      " The humidity will be : " +
-      weatherDaily[1].humidity +
-      "<br>" +
-      "<br>" +
-      "The UV Index will be " +
-      weatherDaily[1].uvi +
-      "</p>"
-  );
-  $("#day-2").append(
-    "<h1>" +
-      moment().add(3, "day").format("dddd") +
-      " " +
-      "</h1>" +
-      "<p id=futureWeather>" +
-      " It will be: " +
-      weatherDaily[2].weather[0].description +
-      "<br>" +
-      "<br>" +
-      "The high will be : " +
-      weatherDaily[2].temp.max +
-      "<br>" +
-      "The low will be : " +
-      weatherDaily[2].temp.min +
-      "<br>" +
-      "<br>" +
-      " The humidity will be : " +
-      weatherDaily[2].humidity +
-      "<br>" +
-      "<br>" +
-      "The UV Index will be " +
-      weatherDaily[2].uvi +
-      "</p>"
-  );
-  $("#day-3").append(
-    "<h1>" +
-      moment().add(4, "day").format("dddd") +
-      " " +
-      "</h1>" +
-      "<p id=futureWeather>" +
-      " It will be: " +
-      weatherDaily[3].weather[0].description +
-      "<br>" +
-      "<br>" +
-      "The high will be : " +
-      weatherDaily[3].temp.max +
-      "<br>" +
-      "The low will be : " +
-      weatherDaily[3].temp.min +
-      "<br>" +
-      "<br>" +
-      " The humidity will be : " +
-      weatherDaily[3].humidity +
-      "<br>" +
-      "<br>" +
-      "The UV Index will be " +
-      weatherDaily[3].uvi +
-      "</p>"
-  );
-  $("#day-4").append(
-    "<h1>" +
-      moment().add(5, "day").format("dddd") +
-      " " +
-      "</h1>" +
-      "<p id=futureWeather>" +
-      " It will be: " +
-      weatherDaily[4].weather[0].description +
-      "<br>" +
-      "<br>" +
-      "The high will be : " +
-      weatherDaily[4].temp.max +
-      "<br>" +
-      "The low will be : " +
-      weatherDaily[4].temp.min +
-      "<br>" +
-      "<br>" +
-      " The humidity will be : " +
-      weatherDaily[4].humidity +
-      "<br>" +
-      "<br>" +
-      "The UV Index will be " +
-      weatherDaily[4].uvi +
-      "</p>"
-  );
-  console.log(weatherDaily[0].weather[0].icon);
-  // var card = $('.forecast-card__cover')[i]
+  for (var day = 0; day < 5; day++) {
+    dayWeather = weatherDaily[day];
 
-  // $(".forecast-card__cover").each(generateBackground());
-  generateBackground();
-}
-
-function generateBackground() {
-  for (i = 0; i < 6; i++) {
-    var icon = weatherDaily[i].weather[0].icon;
+    $(`#day-${day}`).append(
+      `<h1>
+              ${moment()
+                .add(day + 1, "day")
+                .format("dddd")}
+            </h1>
+            <p id=futureWeather>
+                It will be: ${dayWeather.weather[0].description}<br><br>
+                The high will be : ${dayWeather.temp.max}<br><br>
+                The low will be : ${dayWeather.temp.min}<br><br>
+                The humidity will be : ${dayWeather.humidity}<br><br>
+                The UV Index will be ${dayWeather.uvi}
+            "</p>`
+    );
   }
-  if (icon === "01d") {
-    currentCityEl.css(
-      "background-image",
-      "url(./assets/img/01d.gif)"
-    ); //change to sunny day
-  } else if (icon === "01n") {
-    currentCityEl.css(
-      "background-image",
-      "url(./assets/img/01n.gif)"
-    ); //sunny night
-  } else if (icon === "02d") {
-    currentCityEl.css(
-      "background-image",
-      "url(./assets/img/02d.gif)"
-    ); //change to cloudy day
-  } else if (icon === "02n") {
-    currentCityEl.css(
-      "background-image",
-      "url(./assets/img/02n.gif)"
-    ); //change to cloudy day
-  } else if (icon === "03d") {
-    currentCityEl.css(
-      "background-image",
-      "url(./assets/img/03d.gif)"
-    ); //change to scattered cloudy night
-  } else if (icon === "03n") {
-    currentCityEl.css(
-      "background-image",
-      "url(./assets/img/03n.gif)"
-    ); //change to scattered cloudy day
-  } else if (icon === "04d") {
-    currentCityEl.css(
-      "background-image",
-      "url(./assets/img/04d.gif)"
-    ); //change to brokn cloudy day
-  } else if (icon === "04n") {
-    currentCityEl.css(
-      "background-image",
-      "url(./assets/img/04n.gif)"
-    ); //change to brokn cloudy day
-  } else if (icon === "09d") {
-    currentCityEl.css(
-      "background-image",
-      "url(./assets/img/09d.gif)"
-    ); //change to rainy day
-  } else if (icon === "09n") {
-    currentCityEl.css(
-      "background-image",
-      "url(./assets/img/09n.gif)"
-    ); //change to rainy day
-  } else if (icon === "10d") {
-    currentCityEl.css(
-      "background-image",
-      "url(./assets/img/10d.gif)"
-    ); // change to rainy day
-  } else if (icon === "10n") {
-    currentCityEl.css(
-      "background-image",
-      "url(./assets/img/10n.gif)"
-    ); //change to rainy day
-  } else if (icon === "11d") {
-    $currentCityEl.css(
-      "background-image",
-      "url(./assets/img/11d.gif)"
-    ); //change to thunderstorm day
-  } else if (icon === "11n") {
-    currentCityEl.css(
-      "background-image",
-      "url(./assets/img/11n.gif)"
-    ); //change to thunderstorm day
-  } else if (icon === "13d") {
-    currentCityEl.css(
-      "background-image",
-      "url(./assets/img/13d.gif)"
-    ); //change to snowy day
-  } else if (icon === "13n") {
-    currentCityEl.css(
-      "background-image",
-      "url(./assets/img/13n.gif)"
-    ); //change to snowy day
-  } else if (icon === "50d") {
-    $currentCityEl.css(
-      "background-image",
-      "url(./assets/img/50d.gif)"
-    ); //change to misty day
-  } else {
-    currentCityEl.css(
-      "background-image",
-      "url(./assets/img/50n.gif)"
-    ); //change to misty day
+
+  setDayCardBackgrounds();
+}
+function setDayCardBackgrounds() {
+  for (var day = 0; day < 5; day++) {
+    var icon = weatherDaily[day].weather[0].icon;
+    $("#day-" + day).css("background-image", `url(./assets/img/${icon}.gif)`);
   }
 }
 
-// function generateBackground() {
-//   //if weather = sunny {insert sunny background and icon}
-//   //else if weather === rain {insert rainy background}
-//   //else if weather === cloudy {insert cloudy background}
-//   // else if
-// }
+init();
